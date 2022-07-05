e5b60d2d7ac7fd89390bef7ad288b5af11863569
Zhe <zchen@sps-program.com>
2022-07-05 16:45:05 +0000

    Servlet for tips

--- RandomTipServlet.java
+++ RandomTipServlet.java
@@ -8,15 +8,23 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
 
-@WebServlet("/tips")
+@WebServlet("/random-tip")
 public class RandomTipServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     ArrayList<String> tips = new ArrayList<String>();
-    tips.add("Hello!");
-    tips.add("Good evening");
-    tips.add("Good morning");
+    tips.add("Focus on the facts");
+    tips.add("Share your feelings");
+    tips.add("Celebrate your successes");
+    tips.add("Let go of perfectionism");
+    tips.add("Share your failures");
+    tips.add("Cultivate self-compassion");
+    tips.add("Acknowledge, validate, then let go");
+    tips.add("Fighting your feelings with the evidence");
+    tips.add("Reframe your thoughts");
+    tips.add("Learn from your team members");
+    tips.add("Toot your own horn");
     Gson gson = new Gson(); 
     String json = gson.toJson(tips); 
     
